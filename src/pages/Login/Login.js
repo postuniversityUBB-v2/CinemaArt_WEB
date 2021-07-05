9daedfd3ef9fb902262a90c471b6656533bcadaf
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2021-07-05 22:53:20 +0300

    Login with Facebook

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -11,6 +11,8 @@ import Box from "@material-ui/core/Box"
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
 import Typography from "@material-ui/core/Typography"
 import Container from "@material-ui/core/Container"
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 import TypographyCustom from '../../components/components/Typography';
 import AppFooter from '../../components/views/AppFooter';
@@ -52,8 +54,35 @@ function Login() {
     const classes = useStyles();
 	const [email, setEmail] = useState("")
 	const [password, setPassword] = useState("")
+	const [name, setName] = useState("")
 	const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
 	const [done, setDone] = useState(false)
+
+	const responseFacebook = (response) => {
+		console.log(response);
+		setEmail(response.email);
+		setName(response.name);
+
+		localStorage.setItem("token", response.accessToken)
+		localStorage.setItem(
+			"user",
+			JSON.stringify({
+				firstName: name,
+				lastName: name,
+				email: email,
+				token: response.accessToken,
+				role: "User",
+			})
+		)
+		if (response.accessToken !== null) {
+			setDone(true)
+		}
+	}
+
+	const responseGoogle = (response) => {
+		console.log(response);
+	}
+
 	const sendLogin = async e => {
 		e.preventDefault()
 		const { data, status } = await login({
@@ -79,7 +108,7 @@ function Login() {
 	}
 
 	if (user || done) {
-    window.location.href = "/"
+    	window.location.href = "/"
 	}
 
     return (
@@ -134,6 +163,38 @@ function Login() {
 						</Button>
 					</form>
                 </React.Fragment>
+
+				<div className="App" style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+					<div style={{display: "flex", justifyContent: "center"}}>					
+						<FacebookLogin
+							appId="333550634942475" //APP ID NOT CREATED YET
+							fields="name,email,picture"
+							callback={responseFacebook}
+						/>
+					</div>
+
+					{/* <div class="fb-login-button" 
+						data-width="" 
+						data-size="large" 
+						data-button-type="login_with" 
+						data-layout="default" 
+						data-auto-logout-link="true" 
+						data-use-continue-as="true"
+					>
+					</div> */}
+					<br />
+					<br />
+
+					<div style={{display: "flex", justifyContent: "center"}}>	
+						<GoogleLogin
+							clientId="" //CLIENTID NOT CREATED YET
+							buttonText="SIGN IN WITH GOOGLE"
+							onSuccess={responseGoogle}
+							onFailure={responseGoogle}
+						/>
+					</div>
+
+				</div>
               </AppForm>
             <AppFooter />
         </React.Fragment>
